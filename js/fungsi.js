759d8ed8e787715ac0d93acee8111bed82ac04de
n-rizqullah <naupalrizqullah83@gmail.com>
2023-03-18 16:48:41 +0700

    up

---
+++ js/fungsi.js
@@ -0,0 +1,45 @@
+function tambah() {
+    let frm = document.getElementById("calc")
+    let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
+    if (isNaN(bill1) || isNaN(bill2)) {
+        alert("Sorry, you must enter a number!")
+    }else{
+        frm.result.value = bill1 + bill2
+    }
+}
+function kurang() {
+    let frm = document.getElementById("calc")
+    let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
+    if (isNaN(bill1) || isNaN(bill2)) {
+        alert("Sorry, you must enter a number!")
+    }else{
+        frm.result.value = bill1 - bill2
+    }
+}
+function kali() {
+    let frm = document.getElementById("calc")
+    let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
+    if (isNaN(bill1) || isNaN(bill2)) {
+        alert("Sorry, you must enter a number!")
+    }else{
+        frm.result.value = bill1 * bill2
+    }
+}
+function bagi() {
+    let frm = document.getElementById("calc")
+    let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
+    if (isNaN(bill1) || isNaN(bill2)) {
+        alert("Sorry, you must enter a number!")
+    }else{
+        frm.result.value = bill1 / bill2
+    }
+}
+function pangkat() {
+    let frm = document.getElementById("calc")
+    let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
+    if (isNaN(bill1) || isNaN(bill2)) {
+        alert("Sorry, you must enter a number!")
+    }else{   
+        frm.result.value = Math.pow(bill1,  bill2)
+    }
+}

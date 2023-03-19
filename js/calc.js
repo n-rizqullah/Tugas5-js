1fb7539fb0fef28970ff4e255ebc845a239153b2
n-rizqullah <naupalrizqullah83@gmail.com>
2023-03-20 00:52:00 +0700

    perubahan alert

--- js/calc.js
+++ js/calc.js
@@ -2,7 +2,7 @@ function tambah() {
     let frm = document.getElementById("calc")
     let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value)
     if (isNaN(bill1) || isNaN(bill2)) {
-        alert("Sorry, you must enter a number!")
+        alert("Maaf yang Anda Input bukan Angka!")
     }else{
         frm.result.value = bill1 + bill2
     }
@@ -12,7 +12,7 @@ function kurang() {
     let frm = document.getElementById("calc")
     let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
     if (isNaN(bill1) || isNaN(bill2)) {
-        alert("Sorry, you must enter a number!")
+        alert("Maaf yang Anda Input bukan Angka!")
     }else{
         frm.result.value = bill1 - bill2
     }
@@ -22,7 +22,7 @@ function kali() {
     let frm = document.getElementById("calc")
     let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
     if (isNaN(bill1) || isNaN(bill2)) {
-        alert("Sorry, you must enter a number!")
+        alert("Maaf yang Anda Input bukan Angka!")
     }else{
         frm.result.value = bill1 * bill2
     }
@@ -32,7 +32,7 @@ function bagi() {
     let frm = document.getElementById("calc")
     let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
     if (isNaN(bill1) || isNaN(bill2)) {
-        alert("Sorry, you must enter a number!")
+        alert("Maaf yang Anda Input bukan Angka!")
     }else{
         frm.result.value = bill1 / bill2
     }
@@ -42,7 +42,7 @@ function pangkat() {
     let frm = document.getElementById("calc")
     let bill1 = parseFloat(frm.bill1.value), bill2 = parseFloat(frm.bill2.value);
     if (isNaN(bill1) || isNaN(bill2)) {
-        alert("Sorry, you must enter a number!")
+        alert("Maaf yang Anda Input bukan Angka!")
     }else{   
         frm.result.value = Math.pow(bill1,  bill2)
     }
